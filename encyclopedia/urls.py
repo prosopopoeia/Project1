5ef5e6997bba3fb30ecebdfcb7a30a467ed83583
Solomon Grundy <ngwtt@yahoo.com>
2020-08-09 13:58:22 -0600

    added entry edit abilities, package to format markdown notation, fixes here and there

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path("newpage/", views.newpage, name="newpage"),
     path("displaypage/", views.entrypage, name="displaypage"),
     path("wiki/<str:utitle>", views.entrypage, name="entrypage"),
+    path("editpage/<str:utitle>", views.editpage, name="editpage"),
+    path("saveeditpage/", views.saveeditpage, name="saveeditpage"),
+    path("randompage/", views.randompage, name="randompage"),
 ]
